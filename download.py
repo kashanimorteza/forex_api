a7ece3afcb88c20c6c93d0a7f79879f342391017
morteza <kashani.morteza@gmail.com>
2025-11-27 08:35:06 +0330

    ss

--- download.py
+++ download.py
@@ -75,7 +75,6 @@ try:
             if os.path.exists(f"{root_dir}/History"): shutil.rmtree(f"{root_dir}/History")
             time.sleep(5)
             forex.login()
-            if os.path.exists(f"{root_dir}/History"): shutil.rmtree(f"{root_dir}/History")
             datefrom = args.get("datefrom") if args.get("datefrom") not in (None, "") else config['download']['datefrom']
             dateto = args.get("dateto") if args.get("dateto") not in (None, "") else datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
             datefrom = datetime.strptime(datefrom, "%Y-%m-%d %H:%M:%S")
