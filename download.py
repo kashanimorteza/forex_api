5b0a283e860cc4f3b79c514eeda0960563872731
morteza <kashani.morteza@gmail.com>
2025-11-27 08:36:29 +0330

    aa

--- download.py
+++ download.py
@@ -72,8 +72,8 @@ try:
     #--------------params    
     for timeframe in timeframes:
         for instrument in instruments:
-            if os.path.exists(f"{root_dir}/History"): shutil.rmtree(f"{root_dir}/History")
-            time.sleep(5)
+            #if os.path.exists(f"{root_dir}/History"): shutil.rmtree(f"{root_dir}/History")
+            #time.sleep(5)
             forex.login()
             datefrom = args.get("datefrom") if args.get("datefrom") not in (None, "") else config['download']['datefrom']
             dateto = args.get("dateto") if args.get("dateto") not in (None, "") else datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
