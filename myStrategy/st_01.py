a64bea8d57d690f0451fc80d7f71895b0d0f6e18
morteza <kashani.morteza@gmail.com>
2025-12-18 13:35:04 +0330

    ss

--- myStrategy/st_01.py
+++ myStrategy/st_01.py
@@ -182,7 +182,7 @@ class ST_01:
             #--------------Output
             output.time = sort(f"{(time.time() - start_time):.3f}", 3)
             output.data = self.execute_id
-            output.message = f"{action} | {symbol} | {amount} | {tp_pips} | {sl_pips}"
+            output.message = f"{result.status} | {action} | {symbol} | {amount} | {tp_pips} | {sl_pips}"
             #--------------Verbose
             if verbose : self.log.verbose("rep", f"{sort(self.this_class, 8)} | {sort(this_method, 8)} | {output.time}", output.message)
             #--------------Log
